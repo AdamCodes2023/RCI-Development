66112a2d16ff5a85d012881ed8f45cd2933ff37d
AdamCodes2023 <122576141+AdamCodes2023@users.noreply.github.com>
2024-06-27 11:15:24 -0400

    Test the Functionality of the I2C Analog Output Chip

--- RCIMQTT.ino
+++ RCIMQTT.ino
@@ -16,6 +16,7 @@ void setup() {
                       If used battery, please call this function in your project */
   Wire.begin();
 
+  //ADS1115 ADC
   ads1115.begin(0x48);
   ads1115.setGain(GAIN_ONE);
   adc0 = ads1115.readADC_SingleEnded(0);
@@ -23,6 +24,36 @@ void setup() {
 
   M5.Lcd.drawString(String(adc0), 0, 180, 1);
   M5.Lcd.drawString(String(adc1), 0, 210, 1);
+
+  //AD5665R DAC
+  //TURN ON INTERNAL REFRENCE VOLTAGE
+  Wire.beginTransmission(0x1f);
+  Wire.write(byte(56));
+  Wire.write(byte(0));
+  Wire.write(byte(1));
+  Wire.endTransmission();
+
+  //SET ALL CHANNELS TO 0
+  Wire.beginTransmission(0x1f);
+  Wire.write(byte(7));
+  Wire.write(byte(0));
+  Wire.write(byte(0));
+  Wire.endTransmission();
+
+  
+  //TEST WRITE TO AD5665R DAC
+  Wire.beginTransmission(0x1f);
+  Wire.write(byte(0));
+  Wire.write(byte(int(65535) >> 8));
+  Wire.write(byte(int(65535)));
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x1f);
+  Wire.write(byte(1));
+  Wire.write(byte(int(65535) >> 8));
+  Wire.write(byte(int(65535)));
+  Wire.endTransmission();
+  
 }
 
 /* After the program in the setup() function is executed, the program in the loop() function will be executed
