6df0ccae149934b9a6312c04fbd26c52cbb915c7
AdamCodes2023 <122576141+AdamCodes2023@users.noreply.github.com>
2024-06-30 08:49:21 -0400

    Write Functions to Convert a String MAC Address to a Byte Array

--- RCIMQTT.ino
+++ RCIMQTT.ino
@@ -988,6 +988,55 @@ void reconnectMqtt() {
   previousMillis4 = millis();
 }
 
+void hexCharacterStringToBytes(byte *byteArray, const char *hexString) {
+  bool oddLength = strlen(hexString) & 1;
+
+  byte currentByte = 0;
+  byte byteIndex = 0;
+
+  for (byte charIndex = 0; charIndex < strlen(hexString); charIndex++) {
+    bool oddCharIndex = charIndex & 1;
+
+    if (oddLength) {
+      // If the length is odd
+      if (oddCharIndex) {
+        // odd characters go in high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      } else {
+        // Even characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    } else {
+      // If the length is even
+      if (!oddCharIndex) {
+        // Odd characters go into the high nibble
+        currentByte = nibble(hexString[charIndex]) << 4;
+      }
+      else {
+        // Odd characters go into low nibble
+        currentByte |= nibble(hexString[charIndex]);
+        byteArray[byteIndex++] = currentByte;
+        currentByte = 0;
+      }
+    }
+  }
+}
+
+byte nibble(char c) {
+  if (c >= '0' && c <= '9')
+    return c - '0';
+
+  if (c >= 'a' && c <= 'f')
+    return c - 'a' + 10;
+
+  if (c >= 'A' && c <= 'F')
+    return c - 'A' + 10;
+
+  return 0;  // Not a valid hexadecimal character
+}
+
 /* After M5Core2 is started or reset, the program in the setup() function will be executed, and this part will only be executed once. */
 void setup() {
   delay(5000);
@@ -1037,6 +1086,10 @@ void setup() {
   serialNumber = preferences.getString(serialKey);
   stringMac = preferences.getString(macKey);
   preferences.end();
+
+  if (stringMac.indexOf(":") != -1) {
+    hexCharacterStringToBytes(mac, stringMac.substring(0, stringMac.length() - 1).c_str());
+  }
 
   Ethernet.init(26);
   Ethernet.begin(mac);
